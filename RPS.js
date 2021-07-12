41a3b4be4d6cab1a92570d393bf25b7ff7e5f6f2
Kakashka <Ron.liberman.96@gmail.com>
2021-07-12 23:53:05 +0300

    add algorithm2

---
+++ RPS.js
@@ -0,0 +1,8 @@
+//create function named computerPlay
+            // create array that contains string 'Rock', 
+            //'Paper', 'Scissors'
+            // create var randomNum = 0
+            // create function that randomply picks number between 0-2
+                //use method Math.floor((Math.random()*3)+1)
+            // randomNum = that function -1
+            // return string from array at possion randomNumber
